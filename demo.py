f875c64e8f88f10ad9ead4b1e3dc1590bce73660
Yusepp <joseplcam@gmail.com>
2023-02-27 11:26:32 +0100

    Added Plot intensity

--- demo.py
+++ demo.py
@@ -3,10 +3,10 @@ import torch
 import argparse
 import numpy as np
 from PIL import Image
-from time import time
+from time import time, sleep
 from transforms import get_transforms
 from model import SmileDetector
-from utils import detect_face
+from utils import detect_face, get_intensity_plot_array
 
 # Read the arguments
 parser = argparse.ArgumentParser(description='Run Inference RTSD for Smile Detection')
@@ -15,12 +15,15 @@ parser.add_argument('-w', '--weights', type=str, help='Path to trained weights',
 args = parser.parse_args()
 variables = vars(args)
 total = []
+scores = []
+net = variables["weights"].replace(".pth", "")
 
 # Load Model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = SmileDetector(freeze=False)
-model.load_state_dict(torch.load(variables['weights']))
+device = 'mps' if torch.backends.mps.is_available() else device
+model = SmileDetector(freeze=False, net=net)
 model.to(device)
+model.load_state_dict(torch.load(variables['weights'], map_location=torch.device(device)))
 model.eval()
 
 # Get transformations
@@ -28,35 +31,49 @@ tfs, _ = get_transforms(augmentation=False)
 
 # Read webcam loop
 cap = cv2.VideoCapture(0)
+first = True
 while cap.isOpened():
     success, image = cap.read()
+    if first:
+        for i in range(10):
+            success, image = cap.read()
+        first = False
     # Format image
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
     # Detect Faces
     crops, boxes = detect_face(img)    
 
-    # Preprocess
-    start_time = time()
-    crops = tfs(crops).to(device)
-    
-    # Inference
-    smile_scores = model(crops)
-    
-    # Save fps
-    fps = 1/(time() - start_time)
-    total.append(fps)
-    
-    # Detections    
-    for (left, top, right, bottom), score in zip(boxes, smile_scores):
-        cv2.rectangle(image, (int(left), int(top)), (int(right), int(bottom)), (255, 0, 0), 2)
-        cv2.putText(image, f'Smile Score: {score[0]:.4f}',(int(left), int(top) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-    
-    # Print stats
-    cv2.putText(image, f"FPS: {fps:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-    cv2.putText(image, f'Avg. FPS: {np.mean(total):.2f}', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-    cv2.putText(image, f'Max. FPS: {max(total):.2f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
-    cv2.putText(image, f'Min. FPS: {min(total):.2f}', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
+    if len(crops) > 0:
+        # Preprocess
+        start_time = time()
+        crops = tfs(crops).to(device)
+        
+        # Inference
+        smile_scores = model(crops).cpu().detach().numpy()
+        
+        # Save fps
+        fps = 1/(time() - start_time)
+        total.append(fps)
+        
+        
+        
+        # Detections    
+        for (left, top, right, bottom), score in zip(boxes, smile_scores):
+            scores.append(score[0])
+            cv2.rectangle(image, (int(left), int(top)), (int(right), int(bottom)), (255, 0, 0), 2)
+            cv2.putText(image, f'Smile Score: {score[0]:.4f}',(int(left), int(top) - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255), 2)
+        
+        # Print stats
+        cv2.putText(image, f"FPS: {fps:.2f}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (255, 0, 255), 2)
+        cv2.putText(image, f'Avg. FPS: {np.mean(total):.2f}', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (255, 0, 255), 2)
+        cv2.putText(image, f'Max. FPS: {max(total):.2f}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (255, 0, 255), 2)
+        cv2.putText(image, f'Min. FPS: {min(total):.2f}', (10, 160), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (255, 0, 255), 2)
+        
+        intensities = get_intensity_plot_array(scores)
+        intensities = cv2.resize(src=intensities, dsize=(image.shape[1], image.shape[0]))
+
+        
+        image = cv2.vconcat([image, intensities])
     
     # Show image
     cv2.imshow('RTSD: Real-Time Smile Detector', image)
--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@ import numpy as np
 from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates
 from PIL import Image
 
+import matplotlib.pyplot as plt
+import io
+import cv2
+
 mp_face_detection = mp.solutions.face_detection
 
 
@@ -16,25 +20,45 @@ def detect_face(img):
         if not results.detections:
             return crops, boxes
         
-        
-        for det in results.detections:
-            location = det.location_data
-            relative_bounding_box = location.relative_bounding_box
-            rect_start_point = _normalized_to_pixel_coordinates(
-                relative_bounding_box.xmin, relative_bounding_box.ymin, image_cols,
-                image_rows)
-            rect_end_point = _normalized_to_pixel_coordinates(
-                relative_bounding_box.xmin + relative_bounding_box.width,
-                relative_bounding_box.ymin + relative_bounding_box.height, image_cols,
-                image_rows)
-            
-            xleft,ytop=rect_start_point
-            xright,ybot=rect_end_point
+        try:
+            for det in results.detections:
+                location = det.location_data
+                relative_bounding_box = location.relative_bounding_box
+                rect_start_point = _normalized_to_pixel_coordinates(
+                    relative_bounding_box.xmin, relative_bounding_box.ymin, image_cols,
+                    image_rows)
+                rect_end_point = _normalized_to_pixel_coordinates(
+                    relative_bounding_box.xmin + relative_bounding_box.width,
+                    relative_bounding_box.ymin + relative_bounding_box.height, image_cols,
+                    image_rows)
+                
+                xleft, ytop = rect_start_point
+                xright, ybot = rect_end_point
 
-            crops.append(Image.fromarray(img[ytop: ybot, xleft: xright]))
-            boxes.append((xleft, ytop, xright, ybot))
+                crops.append(Image.fromarray(img[ytop: ybot, xleft: xright]))
+                boxes.append((xleft, ytop, xright, ybot))
+        except:
+            return crops, boxes
         
         return crops, boxes
 
-        
-        
+
+def get_intensity_plot_array(values):
+    y = values
+    x = [i for i in range(len(y))]
+    
+    plt.plot(x, y, color='blue')
+    plt.xlabel('Time', fontsize=12)
+    plt.ylabel('Smile Intensity', fontsize=12)
+    plt.ylim(0, 1)
+    fig = plt.gcf()
+    
+    io_buf = io.BytesIO()
+    fig.savefig(io_buf, format='png', dpi=200)
+    io_buf.seek(0)
+    flat_arr = np.frombuffer(io_buf.getvalue(), dtype=np.uint8)
+    img = cv2.imdecode(flat_arr, 1)
+    io_buf.close()
+    
+    return img
+    
